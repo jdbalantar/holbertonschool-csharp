93d72977e9b7461b45759ddc74b039a5e066ee4c
jdbalantar <jdbalantar@gmail.com>
2022-04-11 13:42:49 -0500

    Task 12

---
+++ 0x00-csharp-hello_world/100-hello_error/100-hello_error.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		var error = Console.Error;
+        string str = "and that piece of art is useful - Dora Korpar, 2015-10-19";
+		error.WriteLine(str);
+    }
+}
