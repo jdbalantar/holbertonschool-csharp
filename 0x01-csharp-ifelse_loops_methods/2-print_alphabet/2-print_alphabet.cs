c515a9844121a5ac6a12a40b0bb932cdd2afd609
jdbalantar <jdbalantar@gmail.com>
2022-04-12 12:56:39 -0500

    Adding 'z' char

--- 0x01-csharp-ifelse_loops_methods/2-print_alphabet/2-print_alphabet.cs
+++ 0x01-csharp-ifelse_loops_methods/2-print_alphabet/2-print_alphabet.cs
@@ -6,7 +6,7 @@ namespace _2_print_alphabet
     {
         static void Main(string[] args)
         {
-            for (char c = 'a'; c < 'z'; c++)
+            for (char c = 'a'; c <= 'z'; c++)
                 Console.Write(c);
         }
     }
