d96fb488bc0a2fee35ea8dc43c7fb458be105ddd
jdbalantar <jdbalantar@gmail.com>
2022-04-12 12:50:04 -0500

    Renamed file Program.cs to 6-print_comb2.cs

---
+++ 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _6_print_comb2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    if(i < j){
+                        Console.Write($"{i}{j}");
+                        if(i < 8 && j <= 9)
+                            Console.Write(" ");
+                        if(i == 8 && j == 9)
+                            Console.Write('\n');
+                    }                    
+                }
+            }
+        }
+    }
+}
