ab0c406da8456c1021fe43183677d2d679cf21ae
jdbalantar <jdbalantar@gmail.com>
2022-04-12 12:47:53 -0500

    updating repo

---
+++ 0x01-csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _4_print_hexa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 98; i++)
+                Console.WriteLine($"{i} = 0x{i:x}");
+        }
+    }
+}
---
+++ 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _5_print_comb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 99; i++)
+            {
+                if(i < 10)
+                    Console.Write($"0{i}");
+                if(i >= 10)
+                    Console.Write(i);
+                if(i < 99)
+                    Console.Write(", ");     
+                else
+                    Console.Write('\n');
+            }
+        }
+    }
+}
