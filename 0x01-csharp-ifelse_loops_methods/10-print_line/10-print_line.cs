c33b4299040b5076d6d9eb92687c59125ff857c5
jdbalantar <jdbalantar@gmail.com>
2022-04-12 14:09:51 -0500

    Task 10

---
+++ 0x01-csharp-ifelse_loops_methods/10-print_line/10-print_line.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _10_print_line
+{
+    class Line
+    {
+        public static void PrintLine(int length){
+            for (int i = 0; i < length; i++)
+                Console.Write('_');
+            Console.Write('\n');
+            if (length <= 0)
+                Console.Write('\n');
+        }
+    }
+}
