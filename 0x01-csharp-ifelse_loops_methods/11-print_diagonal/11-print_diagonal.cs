fdb45c9fb4cbed2aa2d7c25b31b486a6cdf6fac9
jdbalantar <jdbalantar@gmail.com>
2022-04-12 16:04:45 -0500

    Updating repo

---
+++ 0x01-csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _11_print_diagonal
+{
+    class Line
+    {
+        public static void PrintDiagonal(int length){
+            int space = 0;
+
+            for(int i = 0; i < length; i++){
+                for (var j = 0; j < length; j++)
+                {
+                    Console.WriteLine("\\");
+                }
+                
+            }
+        }
+    }
+}
