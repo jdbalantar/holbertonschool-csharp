34e8c7484e6d0b87d3b3bb28e2c45439b56c7195
jdbalantar <jdbalantar@gmail.com>
2022-04-12 13:13:36 -0500

    Adding ',' in output

--- 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
+++ 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -13,8 +13,6 @@ namespace _6_print_comb2
                     if(i < j){
                         if(i <= 8 && j <= 9)
                             Console.Write($"{i}{j}, ");
-                        if(i == 8 && j == 9)
-                            Console.Write($"{i}{j}\n");
                     }                    
                 }
             }
