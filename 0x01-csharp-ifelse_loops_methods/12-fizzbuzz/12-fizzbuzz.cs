5c1f262a30f2887ef0914a096353bdaebcd62a82
jdbalantar <jdbalantar@gmail.com>
2022-04-12 17:58:57 -0500

    Task 12 Solved

---
+++ 0x01-csharp-ifelse_loops_methods/12-fizzbuzz/12-fizzbuzz.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _12_fizzbuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+
+        for (n = 1; n <= 100; n ++)
+        {
+            if (n % 3 == 0 && n % 5 == 0)
+                Console.Write("FizzBuzz");
+            else if (n % 3 == 0)
+                Console.Write("Fizz");
+            else if (n % 5 == 0)
+                Console.Write("Buzz");
+            else
+                Console.Write("{0}", n);
+            if (n <= 99)
+                Console.Write(" ");
+            else
+                Console.Write("\n");
+        }
+        }
+    }
+}
