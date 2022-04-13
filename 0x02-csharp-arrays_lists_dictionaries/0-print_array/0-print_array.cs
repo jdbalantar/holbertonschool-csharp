57b2ad256bd20d300b46b50f0b812b093d96d522
jdbalantar <jdbalantar@gmail.com>
2022-04-13 18:02:16 -0500

    Modify class name

--- 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -5,22 +5,28 @@ namespace _0_print_array
     class Array
     {
         public static int[] CreatePrint(int size){
-            if(size == 0)
-                Console.Write('\n');
+            
             if(size < 0){
                 Console.WriteLine("Size cannot be negative");
                 return null;
             }
-                int[] array = new int[size];
-                for (int i = 0; i < size; i++)
-                {
-                    array[i] = i;
-                    Console.Write(array[i]);
-                    if(i < size - 1)
-                        Console.Write(' ');
-                }
-                Console.WriteLine();
+
+            var array = new int[size];
+
+            if(size == 0){
+                Console.Write('\n');
                 return array;
+            }
+                
+            for (int i = 0; i < size; i++)
+            {
+                array[i] = i;
+                Console.Write(array[i]);
+                if(i < size - 1)
+                    Console.Write(' ');
+            }
+            Console.WriteLine();
+            return array;
             
         }
     } 
