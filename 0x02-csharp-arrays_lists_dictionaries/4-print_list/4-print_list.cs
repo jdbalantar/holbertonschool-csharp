cb4bb5d8b2633198a4924a7d40d8286a7379443f
jdbalantar <jdbalantar@gmail.com>
2022-04-13 11:44:07 -0500

    Task 4

---
+++ 0x02-csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _4_print_list
+{
+    class List
+    {
+        public static List<int> CreatePrint(int size)
+        {
+            var NewList = new List<int>();
+            for (int i = 0; i < size; i++){
+                NewList.Add(i);
+                Console.Write(NewList[i]);
+                if(i != size - 1)
+                    Console.Write(' ');
+            }
+            Console.WriteLine("");
+            return NewList;
+        }
+    }
+}
