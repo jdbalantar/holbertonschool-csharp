e16dbdb9e0db71f6af6527e347bf2b06ca3762c6
jdbalantar <jdbalantar@gmail.com>
2022-04-13 10:54:02 -0500

    Task 0

---
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _0_print_array
+{
+    class Arrreglo
+    {
+        public static int[] CreatePrint(int size){
+            if(size == 0)
+                Console.Write('\n');
+            if(size < 0){
+                Console.WriteLine("Size cannot be negative");
+                return null;
+            }
+            else{
+                int[] array = new int[size];
+                for (int i = 0; i < size; i++)
+                {
+                    array[i] = i;
+                    if (i != size - 1)
+                        Console.Write($"{array[i]} ");
+                    else
+                        Console.Write($"{array[i]}");
+                }
+                Console.WriteLine();
+                return array;
+            }
+        }
+    } 
+}
