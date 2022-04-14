79a8529d4143594104732ce4b104f098b92f030c
jdbalantar <jdbalantar@gmail.com>
2022-04-13 22:29:19 -0500

    Task 0

--- 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -4,28 +4,33 @@ class Array
 {
     public static int[] CreatePrint(int size)
     {
-        int[] array;
-        if(size < 0){
-            Console.WriteLine("Size cannot be negative");
-            return (null);
-        }
+        int[] new_array;
+        int i;
 
-        array = new int[size];
-
-        if(size == 0)
-            {
-                Console.WriteLine("");
-                return array;
-            }
-        for (int i = 0; i < size; i++)
+        if (size < 0)
         {
-            array[i] = i;
-            Console.Write(array[i]);
-            if(i != size - 1)
-                Console.Write(" ");
+            Console.WriteLine("Size cannot be negative");
+            return null;
         }
-        Console.WriteLine("");
-        return array;
-        
+
+        new_array = new int[size];
+
+        if (size == 0)
+        {
+            Console.Write("\n");
+            return new_array;
+        }
+
+        for (i = 0; i < size; i++)
+        {
+            new_array[i] = i;
+            Console.Write(new_array[i]);
+            if (i < size - 1)
+                Console.Write(" ");
+                            
+            
+        }
+        Console.Write("\n");
+        return new_array;
     }
 }
