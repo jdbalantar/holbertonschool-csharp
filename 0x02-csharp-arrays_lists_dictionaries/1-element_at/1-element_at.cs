5718e0d3b81ecff0db7ac63d05138638b1cb7f5b
jdbalantar <jdbalantar@gmail.com>
2022-04-13 23:39:02 -0500

    Task 1

---
+++ 0x02-csharp-arrays_lists_dictionaries/1-element_at/1-element_at.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Array
+{
+    public static int elementAt(int[] array, int index)
+    {
+        if (index < 0 || index >= array.Length)
+            return -1;
+        int size = array.Length, value = 0;
+
+        for (var i = 0; i < size; i++)
+        {
+            if (array[i] == array[index])
+                value = array[index];
+        }
+        return (value);
+    }
+}
