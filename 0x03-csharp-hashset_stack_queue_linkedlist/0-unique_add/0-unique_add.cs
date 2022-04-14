23592d0ddc2910902904b2ff8739b7fad0eaa15f
jdbalantar <jdbalantar@gmail.com>
2022-04-14 18:39:26 -0500

    Update repo

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static int Sum(List<int> myList)
+    {
+        var hash = new HashSet<int>(myList);
+        int n = 0;
+        foreach (var i in hash){
+            n += i;
+        }
+    return n;
+    }
+}
