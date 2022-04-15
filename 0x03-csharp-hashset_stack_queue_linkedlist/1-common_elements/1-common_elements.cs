1809df38c17216cee352fdac5c06f39864c71e0b
jdbalantar <jdbalantar@gmail.com>
2022-04-14 20:35:26 -0500

    Task 1

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> CommonElements(List<int> list1, List<int> list2)
+    {
+        var list = list1.FindAll(item => list2.Contains(item));
+        return (list);
+    }
+}
