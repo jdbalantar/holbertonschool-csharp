ea8acad27c76947f067690fda337268a80c2e7af
jdbalantar <jdbalantar@gmail.com>
2022-04-14 21:15:41 -0500

    Fixing checkers

--- 0x03-csharp-hashset_stack_queue_linkedlist/7-linkedlist_add/7-linkedlist_add.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/7-linkedlist_add/7-linkedlist_add.cs
@@ -9,3 +9,27 @@ class LList
         return (myLList.First);
     }
 }
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        LinkedList<int> myLList = new LinkedList<int>();
+
+        myLList.AddLast(1);
+        myLList.AddLast(2);
+        myLList.AddLast(3);
+        myLList.AddLast(4);
+        myLList.AddLast(5);
+
+        foreach (int i in myLList)
+            Console.Write(i + " ");
+        Console.WriteLine();
+
+        LList.Add(myLList, 0);
+
+        foreach (int i in myLList)
+            Console.Write(i + " ");
+        Console.WriteLine();
+    }
+}
