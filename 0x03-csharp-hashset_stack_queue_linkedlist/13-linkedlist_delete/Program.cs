d1a4b11b83f7804b3d139fb6dda40b01d5136de0
jdbalantar <jdbalantar@gmail.com>
2022-04-14 21:05:41 -0500

    Task 13

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static void Delete(LinkedList<int> myLList, int index)
+    {
+        int i;
+        var temp = myLList.First;
+        if (index >= myLList.Count || index < 0)
+            return;
+        for (i = 0; i < index; i++)
+            temp = temp.Next;
+        myLList.Remove(temp);
+    }
+}
