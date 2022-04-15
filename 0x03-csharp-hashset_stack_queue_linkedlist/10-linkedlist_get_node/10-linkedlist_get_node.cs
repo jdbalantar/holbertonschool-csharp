29443e78f2f8edaa2348fbd97cb616946b23810c
jdbalantar <jdbalantar@gmail.com>
2022-04-14 20:59:58 -0500

    Task 10

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+	public static int GetNode(LinkedList<int> myLList, int n)
+	{
+		int ID = 0;
+		foreach (int item in myLList)
+		{
+			if (ID == n)
+				return (item);
+			ID++;
+		}
+		return (0);
+	}
+}
