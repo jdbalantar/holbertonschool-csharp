275ef57f5fcd2e00814800d7eb0faa215c8b4b91
jdbalantar <jdbalantar@gmail.com>
2022-04-14 20:41:39 -0500

    Task 3

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MyStack
+{
+	public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
+	{
+		Console.WriteLine($"Number of items: {aStack.Count}");
+		if (aStack.Count <= 0)
+			Console.WriteLine("Stack is empty");
+		else
+			Console.WriteLine($"Top item: {aStack.Peek()}");
+		if (aStack.Contains(search))
+		{
+			Console.WriteLine($"Stack contains \"{search}\": True");
+			while (aStack.Pop() != search) ;
+		}
+		else
+			Console.WriteLine($"Stack contains \"{search}\": False");
+		aStack.Push(newItem);
+		return aStack;
+	}
+}
