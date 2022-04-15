83b643c3e89e7fb100e956fa60f8f1d4c777fab0
jdbalantar <jdbalantar@gmail.com>
2022-04-14 21:02:52 -0500

    Task 12

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+	public static LinkedListNode<int> Insert(LinkedList<int> myLList, int n)
+	{
+		var temp = myLList.First;
+		while (temp != null)
+		{
+			if (n < temp.Value)
+				return myLList.AddBefore(temp, n);
+			temp = temp.Next;
+		}
+		return myLList.AddLast(n);
+	}
+}
