3a4f229a0459125935f38fa824fb1797ffa800ef
jdbalantar <jdbalantar@gmail.com>
2022-04-14 20:38:29 -0500

    Task 2

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> DifferentElements(List<int> list1, List<int> list2)
+    {
+        var Lista1 = Lista1.FindAll(elem => !Lista2.Contains(elem));
+        var Lista2 = Lista2.FindAll(elem => !Lista1.Contains(elem));
+        Lista1.AddRange(Lista2);
+        Lista1.Sort();
+        return Lista1;
+    }
+}
