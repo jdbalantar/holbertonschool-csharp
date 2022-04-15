b967c4a2de10e1b9d43c8f115023fc84d7a2d413
jdbalantar <jdbalantar@gmail.com>
2022-04-14 20:49:50 -0500

    Task 5

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+	public static LinkedList<int> CreatePrint(int size)
+	{
+        int i;
+		var linkedList = new LinkedList<int>();
+		if (size <= 0)
+			return linkedList;
+		for (i = 0; i < size; i++)
+		{
+			Console.WriteLine(i);
+			linkedList.AddLast(i);
+		}
+		return linkedList;
+	}
+}
