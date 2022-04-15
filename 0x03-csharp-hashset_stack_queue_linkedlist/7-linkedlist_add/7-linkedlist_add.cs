8d78a40c4f468655595524792798e70b858a8b15
jdbalantar <jdbalantar@gmail.com>
2022-04-14 21:12:24 -0500

    Fixing output checkers

--- 0x03-csharp-hashset_stack_queue_linkedlist/7-linkedlist_add/7-linkedlist_add.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/7-linkedlist_add/7-linkedlist_add.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 
 class LList
 {
-	public static LinkedListNode<int> Add(LinkedList<int> myLList, int n)
-	{
-		myLList.AddFirst(n);
-		return (myLList.First);
-	}
+    public static LinkedListNode<int> Add(LinkedList<int> myLList, int n)
+    {
+        myLList.AddFirst(n);
+        return (myLList.First);
+    }
 }
