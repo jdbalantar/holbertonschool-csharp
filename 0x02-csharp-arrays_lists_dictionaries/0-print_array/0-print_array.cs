13cad4e40461f7cbdfae5485b2a93f7de6910d21
jdbalantar <jdbalantar@gmail.com>
2022-04-17 18:22:38 -0500

    Print array

--- 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -1,20 +1,36 @@
 ﻿using System;
 
-class Program
+class Array
 {
-    static void Main(string[] args)
+    public static int[] CreatePrint(int size)
     {
-        int[] newArray;
+        int[] new_array;
+        int i;
 
-        newArray = Array.CreatePrint(10);
-        Console.WriteLine("Array Length: " + newArray.Length);
-        Console.WriteLine("----------------");
-        newArray = Array.CreatePrint(16);
-        Console.WriteLine("Array Length: " + newArray.Length);
-        Console.WriteLine("----------------");
-        newArray = Array.CreatePrint(0);
-        Console.WriteLine("Array Length: " + newArray.Length);
-        Console.WriteLine("----------------");
-        newArray = Array.CreatePrint(-10);
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+
+        new_array = new int[size];
+
+        if (size == 0)
+        {
+            Console.Write("\n");
+            return new_array;
+        }
+
+        for (i = 0; i < size; i++)
+        {
+            new_array[i] = i;
+            Console.Write(new_array[i]);
+            if (i < size - 1)
+                Console.Write(" ");
+                            
+            
+        }
+        Console.Write("\n");
+        return new_array;
     }
 }
