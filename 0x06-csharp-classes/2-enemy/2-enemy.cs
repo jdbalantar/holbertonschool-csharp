9c6f361c011d6945d5b14789087b821c9df0b236
jdbalantar <jdbalantar@gmail.com>
2022-04-26 18:03:44 -0500

    Task 2

---
+++ 0x06-csharp-classes/2-enemy/2-enemy.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace Enemies
+{
+    /// <summary> Empty Class Zombie </summary>
+    public class Zombie
+    {
+        /// <summary>
+        /// Constructor of Zombie class
+        /// </summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+
+        /// <summary>
+        /// public constructor with int parameter
+        /// </summary>
+        /// <param name="value"></param>
+
+        public Zombie(int value){
+            if(value < 0)
+                throw new ArgumentException("Health must be greater than or equal to 0");       
+        }
+
+        /// <summary>
+        /// public int health, no have any value
+        /// </summary>
+        public int health;
+    }
+}
