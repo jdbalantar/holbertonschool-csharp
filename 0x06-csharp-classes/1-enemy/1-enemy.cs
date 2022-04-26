5351f3a3643f2dfdf159dd147c19217d8bedbbd6
jdbalantar <jdbalantar@gmail.com>
2022-04-26 17:47:03 -0500

    Task 1

---
+++ 0x06-csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,17 @@
+﻿namespace Enemies
+{
+    /// <summary> Empty Class Zombie </summary>
+    public class Zombie
+    {
+        /// <summary>
+        /// Constructor of Zombie class
+        /// </summary>
+        public Zombie(){
+            health = 0;
+        }
+        /// <summary>
+        /// public int health, no have any value
+        /// </summary>
+        public int health;
+    }
+}
