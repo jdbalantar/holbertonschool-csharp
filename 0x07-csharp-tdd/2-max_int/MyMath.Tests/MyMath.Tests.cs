9341b671077861a2098f22aa845bba2cdf17e568
jdbalantar <jdbalantar@gmail.com>
2022-04-28 13:23:56 -0500

    Task 2

---
+++ 0x07-csharp-tdd/2-max_int/MyMath.Tests/MyMath.Tests.cs
@@ -0,0 +1,25 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace MyMath.Tests
+{
+    public class Tests
+    {
+        [Test]
+        public void MaxNum()
+        {
+            var array = new int[] { 5, 6, 500000, 87, 71, 3,-500, 50000, 2 };
+            var myList = new List<int>(array);
+            int result = Operations.Max(myList);
+            Assert.AreEqual(500000, result);
+        }
+
+        [Test]
+        public void IsEmpty()
+        {
+            var myList = new List<int>();
+            int result = Operations.Max(myList);
+            Assert.AreEqual(0, result);
+        }
+    }
+}
---
+++ 0x07-csharp-tdd/2-max_int/MyMath/MyMath.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+
+namespace MyMath
+{
+    /// <summary>
+    /// Class that contains math methods
+    /// </summary>
+    public class Operations
+    {
+        /// <summary>
+        /// Method that return max number from an list
+        /// </summary>
+        public static int Max(List<int> nums)
+        {
+            if (nums.Count > 0)
+            {
+                nums.Sort();
+                int num = nums[nums.Count() - 1];
+                return num;
+            }
+            else
+                return 0;
+        }
+    }
+}
