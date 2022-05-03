fa311f92b5d30b3d58a5c06d57a9f6aa8a42d243
jdbalantar <jdbalantar@gmail.com>
2022-05-02 21:17:16 -0500

    Prin properties and methods name

---
+++ 0x08-csharp-inheritance/3-type_get/3-type_get.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Reflection;
+
+/// <summary>
+/// Class Obj
+/// </summary>
+class Obj
+{
+    /// <summary>
+    /// Print Properties and Methods available of an object
+    /// </summary>
+    /// <param name="myObj">object Parameter</param>
+    public static void Print(object myObj)
+    {
+        TypeInfo t = myObj.GetType().GetTypeInfo();
+        Console.WriteLine("{0} Properties:", t.Name);
+        foreach (PropertyInfo prop in t.GetProperties())
+        {
+            Console.WriteLine(prop.Name);
+        }
+        Console.WriteLine("{0} Methods:", t.Name);
+        foreach (MethodInfo meth in t.GetMethods())
+        {
+            Console.WriteLine(meth.Name);
+        }
+    }
+}
