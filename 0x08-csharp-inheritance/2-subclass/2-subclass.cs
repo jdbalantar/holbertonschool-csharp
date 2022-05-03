469626bc7dc2ace5bfb42f97231c78e033287123
jdbalantar <jdbalantar@gmail.com>
2022-05-02 21:15:11 -0500

    Is Subclass

---
+++ 0x08-csharp-inheritance/2-subclass/2-subclass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Obj
+{
+    public static bool IsOnlyASubclass(Type derivedType, Type baseType)
+    {
+        if (derivedType.IsSubclassOf(baseType))
+        {
+            return true;
+        }
+        else
+            return false;
+
+    }
+}
