2de416b3bccbdeb127d1dae25f4c866d0fba63f2
jdbalantar <jdbalantar@gmail.com>
2022-05-02 21:13:50 -0500

    Type object int

---
+++ 0x08-csharp-inheritance/1-instance/1-instance.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+/// <summary>
+///  Class OBJ
+/// </summary>
+class Obj
+{
+    /// <summary>
+    /// Return True or False
+    /// </summary>
+    /// <param name="obj">parameter</param>
+    /// <returns>returns true if is object otherwise returns false</returns>
+    public static bool IsInstanceOfArray(object obj)
+    {
+        if (obj == null)
+            return false;
+        if (typeof(Array).IsInstanceOfType(obj))
+            return true;
+        else
+            return false;
+    }
+}
